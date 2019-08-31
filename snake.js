a55cfaa0f1e2bd43753869396f89da02d00e69df
jennyferbm13 <51867672+jennyferbm13@users.noreply.github.com>
2019-08-30 22:31:37 -0500

    Acomodar

--- snake.js
+++ snake.js
@@ -68,14 +68,7 @@ const winOrLose = function(world){
       processing.state = {
         snake: [{ x: 100, y: 100 }], ancho: 10, alto: 10,
         ultimaTecla: "derecha", score: 0,
-        Obstable1x: 180, Obstacle1y:130, ancho1Obstable: 30, alto1Obstacle: 330,
-        Obstable2x: 460, Obstacle2y:130, ancho2Obstable: 30, alto2Obstacle: 330,
-        Obstable3x: 740, Obstacle3y:130, ancho3Obstable: 30, alto3Obstacle: 330,
-        borde1x: 0,borde1y : 0,borde1ancho : 10,bordealto1 : 600,
-        borde3x: 0,borde3y : 0,borde3ancho : 400,bordealto3 : 10,
-        borde4x: 450,borde4y : 0,borde4ancho : 550,bordealto4 : 10,
-        borde5x: 0,borde5y : 590,borde5ancho : 990,bordealto5 : 10,
-        foodx: (Math.round(Math.random() * 10) / 10) * 800, foody:(Math.round(Math.random() * 10) / 10) * 500, anchof:25, altof:23
+        foodx: (Math.round(Math.random() * 10) / 10) * 800, foody:(Math.round(Math.random() * 10) / 10) * 500, anchof:10, altof:10
         , loser: false
       };
     }
@@ -125,56 +118,35 @@ const winOrLose = function(world){
 
     // Dibuja algo en el canvas. Aqui se pone todo lo que quieras pintar
     processing.drawGame = function (world) {
-      let pintar = function(obj){
-        return processing.rect(obj.x,obj.y,10,10);
+      let pintar = function (obj) {
+        return processing.rect(obj.x, obj.y, 10, 10);
       }
-      let pintarImg= function(imagen,x,y,width,height){
+  
+      let pintarImg = function (imagen, x, y, width, height) {
         return processing.image(imagen, x, y, width, height);
       }
+      pintarImg(img1, 0, 0, 990, 600);
+
+      processing.fill(250, 10, 10);
+      processing.rect(world.foodx, world.foody, 10, 10)
       
-      processing.background(10,200,50);
-      processing.fill(200, 0, 0);
-      pintarImg(img1,0,0,990,600);
-      processing.image(img3,world.foodx, world.foody, 10, 10)
-      processing.fill(40, 140, 200);
-      processing.rect(first(world.snake).x, first(world.snake).y, world.ancho, world.alto);
-      //processing.rect(980,280,10,10);
-      //processing.rect(422,0,10,10)
+  
       processing.fill(0, 0, 0);
       processing.textFont(processing.PFont, 20);
       processing.text("Score: " + world.score, 30, 40);
-      processing.textFont(processing.PFont, 20);
-      processing.text("Best Score: " + actualizar.score2, 30, 60);
-      processing.rect(990,0,10,10);
+      processing.rect(990, 0, 10, 10);
       processing.fill(20, 70, 0);
-      processing.rect(world.Obstable1x, world.Obstacle1y, world.ancho1Obstable, world.alto1Obstacle);
-      processing.fill(20, 70, 0);
-      pintarImg(img2,180,130,30,330);
-      processing.rect(world.Obstable2x, world.Obstacle2y, world.ancho2Obstable, world.alto2Obstacle);
-      processing.fill(20, 70, 0);
-      pintarImg(img2,460,130,30,330);
-      processing.rect(world.Obstable3x, world.Obstacle3y, world.ancho3Obstable, world.alto3Obstacle);
-      processing.fill(20, 70, 0);
-      pintarImg(img2,740,130,30,330);
-      processing.rect(world.borde1x,world.borde1y,world.borde1ancho,world.bordealto1);
-      processing.fill(20, 70, 0);
-      pintarImg(img2,0,0,10,600);
-      processing.rect(world.borde3x,world.borde3y,world.borde3ancho,world.bordealto3);
-      processing.fill(20, 70, 0);
-      pintarImg(img2,0,0,400,10);
-      processing.rect(world.borde4x,world.borde4y,world.borde4ancho,world.bordealto4);
-      processing.fill(20, 70, 0);
-      pintarImg(img2,450,0,550,10);
-      processing.rect(world.borde5x,world.borde5y,world.borde5ancho,world.bordealto5);
-      processing.fill(240, 240, 240);
-      pintarImg(img2,0,590,990,10);
-      processing.rect(world.cuadro1x,world.cuadro1y,world.cuadro1ancho,world.cuadro1alto);
-      processing.fill(240, 240, 240);
-      processing.rect(world.cuadro2x,world.cuadro2y,world.cuadro2ancho,world.cuadro2alto);
-      processing.fill(240, 240, 240);
-      mapObj(world.snake,pintar);
-      
-      
+  
+      pintarImg(img2, 180, 130, 30, 330);
+      pintarImg(img2, 460, 130, 30, 330);
+      pintarImg(img2, 740, 130, 30, 330);
+      pintarImg(img2, 0, 0, 10, 600);
+      pintarImg(img2, 0, 0, 400, 10);
+      pintarImg(img2, 450, 0, 550, 10);
+      pintarImg(img2, 0, 590, 990, 10);
+  
+      processing.fill(250, 250,250);
+      mapObj(world.snake, pintar);
     }
    
 
