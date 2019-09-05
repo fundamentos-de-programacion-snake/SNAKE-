1556dfa6520d1bf281d485542ea7667a5d97a9ad
Marcela-G <l.m.guamanga@gmail.com>
2019-09-05 16:05:57 -0500

    Elimine lo del otro taller

--- taller11copy/src/RecursionGenerativa.js
+++
@@ -1,9 +0,0 @@
-const { last, mayor, principio, invertir, listaResto, ordenar, fibLista, fib, eliminar,
-    añadir, longitud, posicion, posicionDes, desordenar, aplicar, buscarElemento, borrarElemento,
-    mayorQue,par, condicionPar,map,min
-} = require('../src/ejercicioTaller');
-const { cons, first, rest, isEmpty, isList } = require('functional-light');
-
-const itemsLess = function(puntero,list){
-    if(isEmpy)
-}
