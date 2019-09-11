ca99d93264ce72368cf2172e9035e9337bd3ca99
jennyferbm13 <51867672+jennyferbm13@users.noreply.github.com>
2019-09-10 20:04:50 -0500

    documentacion

--- functionsMovement.js
+++ functionsMovement.js
@@ -1,194 +1,160 @@
-/* Funciones de movimiento
-/** Para definir funciones de movimiento es necesario primero definir el mundo a trabajar:
- *     processing.draw = function () {
-      /**
-       *Dibuja la serpiente.
-      function drawSnake(snake) {
-        snake.forEach(part => {
-          processing.fill(240, 240, 240);
-          processing.rect(10,10, 10,10);
-        });
-      }
-MoveRight : world -> world;
-Proposito: Diseñar una funcion que permita a la serpiente empezar a moverse
-hacia la derecha en un espacio bidimensional.
-Ejemplos: 
-moveRight (snake) -> snake.x = 20.
-Cuerpo:
-*/
-const moveRight = function(world){
+/**
+ * Diseñar una funcion que permita a la serpiente empezar a moverse
+ * hacia la derecha en un espacio bidimensional.
+ * @param {world} world 
+ * @returns {number}
+ * @example moveRight (snake) -> snake.x = 20.
+ */
+const moveRight = function (world) {
   return world.x + 10
 }
-/** Pruebas
-* console.log (snake.x);
-*/
 
-/*
-MoveUp : world -> world;
-Proposito: Diseñar una funcion que permita a la serpiente empezar a moverse
-hacia arriba en un espacio bidimensional.
-Ejemplos: 
-moveUp (snake) -> snake.y = 20.
-Cuerpo:
-*/
-const moveUp = function(world){
+/**
+ * Diseñar una funcion que permita a la serpiente empezar a moverse
+ * hacia arriba en un espacio bidimensional.
+ * @param {world} world 
+ * @returns {number}
+ * @example moveUp (snake) -> snake.y = 0.
+ */
+const moveUp = function (world) {
   return world.y - 10;
 }
-/** Pruebas
- * console.log (snake.y);
+
+/**
+ * Diseñar una funcion que permita a la serpiente empezar a moverse
+ * hacia abajo en un espacio bidimensional.
+ * @param {world} world 
+ * @returns {number}
+ * @example moveDown (snake) -> snake.y = 20.
  */
-/*
-MoveDown : world -> world;
-Proposito: Diseñar una funcion que permita a la serpiente empezar a moverse
-hacia abajo en un espacio bidimensional.
-Ejemplos: 
-moveDown (snake) -> snake.y = 0.
-Cuerpo:
-*/
-const moveDown = function(world){
-  return  world.y + 10
+const moveDown = function (world) {
+  return world.y + 10
 }
-/** Pruebas
- * console.log (snake.y);
+
+/**
+ * Proposito: Diseñar una funcion que permita a la serpiente empezar a moverse
+ * hacia la izquierda en un espacio bidimensional.
+ * @param {world} world 
+ * @returns {number}
+ * @example moveLeft (snake) -> snake.x = 0
  */
- /*
-MoveLeft : world -> world;
-Proposito: Diseñar una funcion que permita a la serpiente empezar a moverse
-hacia la izquierda en un espacio bidimensional.
-Ejemplos: 
-moveLeft (snake) -> snake.x = 0.
-Cuerpo:
-*/
-const moveLeft = function(world){
+const moveLeft = function (world) {
   return world.x - 10;
 }
-/** Pruebas
- * console.log (snake.x);
- */
 
- /*
-MapObjX : list, function -> list;
-Proposito: Diseñar una funcion que reciba una lista y una funcion y permita aplicar la misma
-al atributo "x" de los elementos de la lista. 
-Ejemplos: 
-mapObjX (snake, principio) -> snake.x = 10.
-Cuerpo:
-*/
-const mapObjX = function(list,fx){
-  return cons({x: fx(first(list)), y:first(list).y},principio(list));
-}
-/** Pruebas
- * console.log (snake.x);
- */
-/*
-MapObjY : list, function -> list;
-Proposito: Diseñar una funcion que reciba una lista y una funcion y permita aplicar la misma
-al atributo "y" de los elementos de la lista. 
-Ejemplos: 
-mapObjX (snake, principio) -> snake.y = 10.
-Cuerpo:
-*/
-const mapObjY = function(list,fx){
-  return cons({x: first(list).x, y: fx(first(list))},principio(list));
-}
-/** Pruebas
- * console.log (snake.y);
- */
-
- /*
-MapObj : list, function -> list;
-Proposito: Diseñar una funcion que reciba una lista y una funcion y permita aplicar la misma
-a cada uno de los elementos de la lista. 
-Ejemplos: 
-mapObjX (snake, principio) -> snake.y = 10.
-mapObjY (snake, principio) -> snake.x = 10.
-Cuerpo:
-*/
-const mapObj = function(list,fx){
-  if(isEmpty(list)){
-    return [];
-  }else{
-    return cons(fx(first(list)),mapObj(rest(list),fx));
-  }
-}
-/** Pruebas
- * console.log (snake.y);
- * console.log (snake.x)
- */
-
-
- /** movementOutScreenUp : world -> world.
-  * Proposito: Elaborar una funcion que reciba un mundo y permita determinar cuando snake se mueve hacia afuera
-  * de la pantalla por la parte superior.
-  * Cuerpo:
-  */
- const movementOutScreenUp = function(world){
-  return make(world, {snake: cons({x: 980 , y: 280 },principio(world.snake)),ultimaTecla: "izquierda"})
-}
-
- /** movementOutScreenRight : world -> world.
-  * Proposito: Elaborar una funcion que reciba un mundo y permita determinar cuando snake se mueve hacia afuera
-  * de la pantalla por la parte superior.
-  * Cuerpo:
-  */
-const movementOutScreenRight = function(world){
-  return make(world, {snake: cons({x: 420, y: 10},principio(world.snake)), ultimaTecla: "abajo"})
-}
-
-/** OutOfScreenRight : world -> world
- * Proposito: Elaborar una funcion de reciba un mundo y evalue si snake se movio hacia el borde del mapa
- * por la derecha, para aplicar el metodo "movementOutScreenRight". En caso contrario, devuelve el mundo
- * sin alteraciones.
- * Cuerpo:
- */
- const outOfScreenRight = function(world){
-    if(first(world.snake).x >= 980 && world.ultimaTecla == "derecha"){
-      return movementOutScreenRight(world);
-    }else{
-      return make(world,{});
-    }
-  }
-  /** OutOfScreenUp : world -> world
- * Proposito: Elaborar una funcion de reciba un mundo y evalue si snake se movio hacia el borde del mapa
- * por arriba, para aplicar el metodo "movementOutScreenRight". En caso contrario, devuelve el mundo
- * sin alteraciones.
- * Cuerpo:
- */
-  const OutOfScreenUp = function(world){
-    if(first(world.snake).y<10 && first(world.snake).x>=400 && first(world.snake).x <=445 && world.ultimaTecla=="arriba"){
-      return movementOutScreenUp(world);
-    }else{
-      return make(world,{});  
-    }
-  }
-      /**
-     * Contrato: world->world
-     * Propósito: Función que decide en qué dirección dibujar la serpiente dependiendo de la
-     * última tecla.
-     * Cuerpo:
-     */
-
-    const mover = function (world) {
-      if (world.ultimaTecla === "derecha") {
-        return make(world, { snake: mapObjX(world.snake,moveRight) });
-      }
-      if (world.ultimaTecla === "arriba") {
-        return make(world, { snake: mapObjY(world.snake,moveUp) });
-      }
-      if (world.ultimaTecla === "abajo") {
-        return make(world, { snake: mapObjY(world.snake,moveDown) });
-      }
-      if (world.ultimaTecla === "izquierda") {
-        return make(world, { snake: mapObjX(world.snake,moveLeft) });
-      }
-      return make(world,{});
-    }
 /**
- * mover2 : world -> world
- * Proposito : Mover de maneja constante hacia la izquierda los obstaculos "bala"
- * Ejemplo : mover2(world) -> world
+ * Diseñar una funcion que reciba una lista y una funcion ,que permita aplicar la misma
+ * al atributo "x" de los elementos de la lista. 
+ * @param {lista} list 
+ * @param {funcion} fx 
+ * @returns {list}
  */
-  
-  const mover2 = function (world){
-    return make(world, { bala: mapObjX(world.bala,moveLeft),bala1:mapObjX(world.bala1,moveLeft),bala2: mapObjX(world.bala2,moveLeft) });
+const mapObjX = function (list, fx) {
+  return cons({ x: fx(first(list)), y: first(list).y }, principio(list));
+}
+
+/**
+ * Diseñar una funcion que reciba una lista y una funcion ,que permita aplicar la misma
+ * al atributo "y" de los elementos de la lista. 
+ * @param {lista} list 
+ * @param {funcion} fx 
+ * @returns {list}
+ */
+const mapObjY = function (list, fx) {
+  return cons({ x: first(list).x, y: fx(first(list)) }, principio(list));
+}
+
+/**
+ * Diseñar una funcion que reciba una lista y una funcion ,que permita aplicar la misma
+ * a cada uno de los elementos de la lista. 
+ * @param {lista} list 
+ * @param {funcion} fx 
+ * @returns {list}
+ */
+const mapObj = function (list, fx) {
+  if (isEmpty(list)) {
+    return [];
+  } else {
+    return cons(fx(first(list)), mapObj(rest(list), fx));
   }
+}
+
+/**
+ * Hacer que el snake salga por la parte derecha del canvas y se mueva hacia la izquierda
+ * @param {world} world 
+ * @returns {world}
+ */
+
+const movementOutScreenUp = function (world) {
+  return make(world, { snake: cons({ x: 980, y: 280 }, principio(world.snake)), ultimaTecla: "izquierda" })
+}
+
+/** 
+* Hacer que el snake salga por la parte superios del canvas y se mueva hacia abajo
+* @param {world} world 
+* @returns {world}
+*/
+const movementOutScreenRight = function (world) {
+  return make(world, { snake: cons({ x: 420, y: 10 }, principio(world.snake)), ultimaTecla: "abajo" })
+}
+
+
+/**
+ * Elaborar una funcion de reciba un mundo y evalue si snake se movio hacia el borde del mapa
+ * por la derecha, para aplicar el metodo "movementOutScreenRight.
+ * @param {world} world 
+ * @returns {world}
+ */
+const outOfScreenRight = function (world) {
+  if (first(world.snake).x >= 980 && world.ultimaTecla == "derecha") {
+    return movementOutScreenRight(world);
+  } else {
+    return make(world, {});
+  }
+}
+
+
+/**
+ * Elaborar una funcion de reciba un mundo y evalue si snake se movio hacia el borde del mapa
+ * por arriba, para aplicar el metodo "movementOutScreenRight".
+ * @param {world} world 
+ * @returns {world}
+ */
+const OutOfScreenUp = function (world) {
+  if (first(world.snake).y < 10 && first(world.snake).x >= 400 && first(world.snake).x <= 445 && world.ultimaTecla == "arriba") {
+    return movementOutScreenUp(world);
+  } else {
+    return make(world, {});
+  }
+}
+
+/**
+ * Función que decide en qué dirección se mueve la serpiente dependiendo de la
+  * última tecla.
+ * @param {world} world 
+ * @returns {world}
+ */
+const mover = function (world) {
+  if (world.ultimaTecla === "derecha") {
+    return make(world, { snake: mapObjX(world.snake, moveRight) });
+  }
+  if (world.ultimaTecla === "arriba") {
+    return make(world, { snake: mapObjY(world.snake, moveUp) });
+  }
+  if (world.ultimaTecla === "abajo") {
+    return make(world, { snake: mapObjY(world.snake, moveDown) });
+  }
+  if (world.ultimaTecla === "izquierda") {
+    return make(world, { snake: mapObjX(world.snake, moveLeft) });
+  }
+  return make(world, {});
+}
+
+/**
+ * Mover de maneja constante hacia la izquierda los obstaculos "bala"
+ * @param {world} world 
+ * @returns {world}
+ */
+const mover2 = function (world) {
+  return make(world, { bala: mapObjX(world.bala, moveLeft), bala1: mapObjX(world.bala1, moveLeft), bala2: mapObjX(world.bala2, moveLeft) });
+}
