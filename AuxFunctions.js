d4091a4b5c126c3014fbd8e14cdb55d995574b2b
jennyferbm13 <51867672+jennyferbm13@users.noreply.github.com>
2019-09-10 21:03:29 -0500

    documentacion

--- AuxFunctions.js
+++ AuxFunctions.js
@@ -1,10 +1,9 @@
 /**
- * Propósito: Función que retorna la lista dada sin su último elemento.
+ * Función que retorna la lista dada sin su último elemento.
  * @param {list} list 
  * @returns {list}
- * Ejemplos: 
- * [4,2,7,3] => [4,2,7]
- * [4,0,4,2] => [4,0,4]
+ * @example principio ([4,2,7,3]) // => [4,2,7]
+ * @example principio ([4,0,4,2]) // => [4,2,7]
  */
 const principio = function (list) {
   if (isEmpty(list)) {
@@ -16,11 +15,11 @@ const principio = function (list) {
     return cons(first(list), principio(rest(list)));
   }
 }
+
 /**
- * Propósito:
- Determinar una funcion que nos arroje un booleano
- para establecer si un determinado objeto rect, se encuentra 
- dentro de un determinado objeto p.
+ * Determinar una funcion que nos arroje un booleano
+ * para establecer si un determinado objeto rect, se encuentra 
+ * dentro de un determinado objeto p.
  * @param {object} rect 
  * @param {object} p 
  * @returns {boolean}
@@ -31,14 +30,12 @@ function isInside(rect, p) {
 }
 
 /**
- * Propósito: 
  * Elaborar una funcion anonima que dada una lista,
   retorne el ultimo elemento de la misma.
  * @param {list} list 
  * @returns {number}
- * Ejemplos:
- * [3,4,5,6,2,1] => 1
- * [1,2,3,4,5,6,7] => 7
+ * @example last ([3,4,5,6,2,1]) // => 1
+ * @example last ([1,2,3,4,5,6,7]) // => 7
  */
 const last = function (list) {
   if (isEmpty(list)) {
@@ -49,16 +46,15 @@ const last = function (list) {
     return last(rest(list));
   }
 }
+
 /**
- * Propósito:
  * Diseñar una funcion que reciba una lista y devuelva el valor de la longitud de la
   lista contando cada uno de sus elementos.
  * @param {list} list 
  * @returns {number}
- * Ejemplos:
- * ["a",4,7,"a"] => 4
- * ["p",7,2] => 3
- * [] => 0 
+ * @example longitud (["a",4,7,"a"]) // => 4
+ * @example longitud (["p",7,2]) // => 3
+ * @example longitud ([]) // => 0
  */
 const longitud = function (list) {
   if (isEmpty(list)) {
@@ -68,45 +64,40 @@ const longitud = function (list) {
   }
 }
 /**
- * Propósito: 
  * Diseñar una funcion que reciba dos objetos de atributos "x", "y" y devuelva el valor
  * de la distancia euclidiana entre ambos.
  * @param {object} a 
  * @param {object} b
  * @returns {number} 
- * Ejemplo:
+ * @example l2 (obj2,obj1) // => 14,14...
  * let obj1 = {x: 20, y: 20}
  * let obj2 = {x: 10 , y: 10}
- * l2(obj2,obj1) => 14,14... 
  */
 function l2(a, b) {
   return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
 }
+
 /**
- * Propósito:
  * Diseñar una funcion que reciba la informacion de dos parametros y la asigne a un nuevo objeto
  * mediante el metodo "Object.assign".
  * @param {object} data 
  * @param {object} attribute 
  * @returns {object}
- * Ejemplo:
+ * @example make (any1, any2) // => {x: 20, y:10, z: 10}
  * let any1 = {x: 20, y: 10}
  * let any2 = {y: 10 , z: 10}
- * make (any1, any2); -> {x: 20, y:10, z: 10}
  */
-
 function make(data, attribute) {
   return Object.assign({}, data, attribute);
 }
+
 /**
- * Propósito:
  * Diseñar una funcion que permita concatenar dos listas en una nueva lista 
  * con los elementos de las anteriores.
  * @param {list} x 
  * @param {list} y 
- * Ejemplo:
- * append([4,2],[1,0]) => [4,2,1,0]
- * append([0,7,8,5],[1,10,4]) => [0,7,8,5,1,10,4]
+ * @example append([4,2],[1,0]) // => [4,2,1,0]
+ * @example append([0,7,8,5],[1,10,4]) // => [0,7,8,5,1,10,4]
  */
 function append(x, y) {
   if (isEmpty(x)) {
